eee1e58848ba835ab43800be545ee867a61a2a27
Hüseyin Erdem ÖZKAYA <36443511+erdemozkaya@users.noreply.github.com>
2021-08-23 13:24:12 +0300

    Update base-service.ts

--- src/app/core/common/base-service.ts
+++ src/app/core/common/base-service.ts
@@ -2,14 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, Injector } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { AppInjectorService } from './app-injector.service';
+import { BaseService2SubjectService } from "./base-service2-subject";
 
 @Injectable({
     providedIn: "root",
 })
 export class BaseService {
-    public loadingSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
-        true
-    );
     protected httpClient:HttpClient;
 
     constructor() {
@@ -19,21 +17,28 @@ export class BaseService {
             debugger
         }
     }
+  
+    private setSubject(showLoadingTemp:boolean){
+        BaseService2SubjectService.setSubject(showLoadingTemp);
+    }
 
     post<T,R>(filterHelper: T, addtionalUrl, showLoadingTemp = true): Observable<R> {
-        this.loadingSubject.next(showLoadingTemp);
+        this.setSubject(showLoadingTemp);
         return this.httpClient.post(`${addtionalUrl}`,filterHelper) as Observable<R>;
     }
 
     create<T,R>(model:T | FormData,addtionalUrl, showLoadingTemp = true): Observable<R>{
+        this.setSubject(showLoadingTemp);
         return this.httpClient.post(`${addtionalUrl}`, model) as Observable<R>;
     }
 
     update<T,R>(model: T | FormData,addtionalUrl="update", showLoadingTemp = true): Observable<R> {
+        this.setSubject(showLoadingTemp);
         return this.httpClient.put(`${addtionalUrl}`, model) as Observable<R>;
     }
 
     delete<R>(id: number,addtionalUrl="deletebyid", showLoadingTemp = true): Observable<R> {
+        this.setSubject(showLoadingTemp);
         return this.httpClient.delete(`/${addtionalUrl}?entityId=` + id) as Observable<R>;
     }
 }
